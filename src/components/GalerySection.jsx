7fb0506a08cae1b6237d5e855a68b5ebfb423524
Daniel M Santos <danielmelosantos5901@gmail.com>
2026-05-29 11:38:44 -0300

    Galeria de fotos

--- src/components/GalerySection.jsx
+++ src/components/GalerySection.jsx
@@ -1,4 +1,22 @@
+import evento1 from "../assets/gallery/evento1.jpeg"
+import evento2 from "../assets/gallery/evento2.jpeg"
+import evento3 from "../assets/gallery/evento3.jpeg"
+import evento4 from "../assets/gallery/evento4.jpeg"
+import evento5 from "../assets/gallery/evento5.jpeg"
+import evento6 from "../assets/gallery/evento6.jpeg"
+
+
 function GallerySection() {
+
+  const images = [
+  evento1,
+  evento2,
+  evento3,
+  evento4,
+  evento5,
+  evento6,
+]
+
   return (
     <section className="px-6 lg:px-20 py-24 bg-zinc-900">
       <div className="max-w-6xl mx-auto">
@@ -14,12 +32,15 @@ function GallerySection() {
           Fotos e vídeos reais dos eventos atendidos pela Equipe Open Bar Mogi.
         </p>
 
-        <div className="grid md:grid-cols-4 gap-4">
-          <div className="md:col-span-2 md:row-span-2 h-80 bg-zinc-800 rounded-2xl border border-zinc-700" />
-          <div className="h-40 bg-zinc-800 rounded-2xl border border-zinc-700" />
-          <div className="h-40 bg-zinc-800 rounded-2xl border border-zinc-700" />
-          <div className="h-40 bg-zinc-800 rounded-2xl border border-zinc-700" />
-          <div className="h-40 bg-zinc-800 rounded-2xl border border-zinc-700" />
+        <div className="grid md:grid-cols-3 gap-6">
+          {images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt={`Evento ${index + 1}`}
+              className="w-full h-72 object-cover rounded-2xl border border-zinc-700 hover:scale-105 transition duration-300"
+            />
+          ))}
         </div>
       </div>
     </section>
